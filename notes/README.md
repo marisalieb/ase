e4fa8ee5ca802deef71984ba8401cdbe5d98be86
Liebner Marisa <s5723321@w12002.student.bournemouth.ac.uk>
2024-09-30 15:26:44 +0100

    restructure into notes folder

---
+++ notes/README.md
@@ -0,0 +1,6 @@
+# Notes from the first ASE lecture
+
+nicerhelloworld is how helloworld should be written
+
+and then noteslecture.md is a copy of all the linux commands used in the class and then a few more after the class separated bz several spaces
+
---
+++ notes/nicerhelloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib> // has EXIT_SUCCESS
+
+int main()
+{
+    std::cout<< "hello world \n"; // better not to copy all of stdlib and just use std:: and \n instead of endl since performance is better
+    return EXIT_SUCCESS; // exit_success makes it clearer instead of using 0
+
+}
